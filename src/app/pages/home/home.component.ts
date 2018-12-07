27607fb73357149c3d1adedabb8307f52e0507df
iEPHYP <eric.maulabi@gmail.com>
2018-12-07 22:11:05 +0100

    added disposing services' streams

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DataService } from './home.data.service';
+import { RandomerService } from './home.randomer.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent implements OnDestroy {
+  constructor(
+    private randomerService: RandomerService,
+    private dataService: DataService
+  ) {}
 
-  ngOnInit() {}
+  ngOnDestroy(): void {
+    this.randomerService.destroy();
+    this.dataService.destroy();
+  }
 }
