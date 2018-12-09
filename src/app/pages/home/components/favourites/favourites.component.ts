426a92f0c571eeb36453fd079a08c38e9e0fc81e
iEPHYP <eric.maulabi@gmail.com>
2018-12-09 22:00:52 +0100

    destructured ngOnInit hooks for primary setup methods. Injected favourites service for each jokes list and managed communication between them. Moved logic of limitation of 10 favourites jokes to the favourites component.

--- src/app/pages/home/components/favourites/favourites.component.ts
+++ src/app/pages/home/components/favourites/favourites.component.ts
@@ -3,6 +3,7 @@ import { Joke } from '../jokes/jokes.model';
 import { RandomerService } from '../../home.randomer.service';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+import { FavouriteJokesService } from '../../home.favourite-jokes.service';
 
 @Component({
   selector: 'app-favourites',
@@ -14,15 +15,63 @@ export class FavouritesComponent implements OnInit, OnDestroy {
   public jokes: Joke[] = [];
   private destroyed = new Subject();
 
-  constructor(private randomerService: RandomerService) {}
+  private jokesLimit = 10;
+
+  constructor(
+    private randomerService: RandomerService,
+    private favouriteJokesService: FavouriteJokesService
+  ) {}
 
   ngOnInit() {
-    this.randomerService
-      .getRandomJoke$(this.jokes)
+    this.setupFavouriteSyncing();
+    this.setupRandomingTimer();
+  }
+
+  setupFavouriteSyncing() {
+    this.jokes = this.favouriteJokesService.getFavouriteJokes();
+
+    this.favouriteJokesService
+      .getStarChange$()
       .pipe(takeUntil(this.destroyed))
       .subscribe(joke => {
-        joke && this.jokes.push(joke);
+        let listMutated = false;
+
+        const _jokeIndex = this.jokes.findIndex(j => j.id === joke.id);
+        const _joke = _jokeIndex >= 0 && this.jokes[_jokeIndex];
+        if (!_joke && joke.liked) {
+          const iCould = this.addJokeIfYouCan(joke);
+          listMutated = true;
+          if (!iCould) {
+            joke.liked = false;
+            this.update(joke);
+            listMutated = false;
+          }
+        } else if (_joke && !joke.liked) {
+          this.jokes.splice(_jokeIndex, 1);
+          listMutated = true;
+        }
+
+        listMutated &&
+          this.favouriteJokesService.updateFavouritesInStorage(this.jokes);
+        // this is for the case when prop jokes is map
       });
+  }
+
+  setupRandomingTimer() {
+    this.randomerService
+      .getRandomJoke$()
+      .pipe(takeUntil(this.destroyed))
+      .subscribe(joke => {
+        joke && this.update(joke);
+      });
+  }
+
+  update(joke: Joke) {
+    this.favouriteJokesService.update(joke);
+  }
+
+  addJokeIfYouCan(joke: Joke) {
+    return this.jokes.length < this.jokesLimit && this.jokes.push(joke);
   }
 
   ngOnDestroy(): void {
--- src/app/pages/home/components/random-jokes/random-jokes.component.ts
+++ src/app/pages/home/components/random-jokes/random-jokes.component.ts
@@ -4,6 +4,7 @@ import { Joke } from '../jokes/jokes.model';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { DataService } from '../../home.data.service';
+import { FavouriteJokesService } from '../../home.favourite-jokes.service';
 
 @Component({
   selector: 'app-random-jokes',
@@ -15,23 +16,49 @@ export class RandomJokesComponent implements OnInit, OnDestroy {
   public loading: boolean = true;
   public destroyed = new Subject();
 
-  constructor(private service: DataService) {}
+  constructor(
+    private dataService: DataService,
+    private favouriteJokesService: FavouriteJokesService
+  ) {}
 
   ngOnInit() {
-    this.service
+    this.setupDataFetch();
+    this.setupFavouriteSyncing();
+  }
+
+  setupDataFetch() {
+    this.dataService
       .getRefresher$()
       .pipe(takeUntil(this.destroyed))
       .subscribe(() => {
         this.loading = true;
       });
 
-    this.service
+    this.dataService
       .getRandomJokes()
       .pipe(takeUntil(this.destroyed))
       .subscribe(data => {
         this.jokes = data.value;
+        // this.jokes = this.favouriteJokesService.syncWithFavourites(data.value); // TODO: realize this reactivity
         this.loading = false;
       });
+  }
+
+  setupFavouriteSyncing() {
+    this.favouriteJokesService
+      .getStarChange$()
+      .pipe(takeUntil(this.destroyed))
+      .subscribe(joke => {
+        const _joke = this.jokes.find(j => j.id === joke.id);
+        _joke && (_joke.liked = joke.liked);
+        // this is for the case when prop jokes is map
+        // const j = this.jokes[joke.id];
+        // j && (j.stared = joke.stared);
+      });
+  }
+
+  update(joke: Joke) {
+    this.favouriteJokesService.update(joke);
   }
 
   ngOnDestroy() {
