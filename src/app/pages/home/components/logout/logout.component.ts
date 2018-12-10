a091b31671ada886be163ebcc86dabafddf6f28f
iEPHYP <eric.maulabi@gmail.com>
2018-12-10 16:29:49 +0100

    added handler for logout click

--- src/app/pages/home/components/logout/logout.component.ts
+++ src/app/pages/home/components/logout/logout.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/pages/login/login.service';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.scss']
 })
-export class LogoutComponent implements OnInit {
+export class LogoutComponent {
 
-  constructor() { }
+  constructor(public loginService: LoginService) { }
 
-  ngOnInit() {
+  onClick() {
+    this.loginService.logout();
   }
-
 }
