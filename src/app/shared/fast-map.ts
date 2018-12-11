afeb875d98e27ea5e60c0155d532c60a3abe1f65
iEPHYP <eric.maulabi@gmail.com>
2018-12-11 17:55:46 +0100

    added some adjustment for FastMap. Checking for existance while inserting and adjusting indexes after removal. And changed FastMap by default using 'id' as a key if key didn't specified.

--- src/app/shared/fast-map.ts
+++ src/app/shared/fast-map.ts
@@ -4,24 +4,67 @@
  * O(1) read
  * O(1) insert
  * O(1) update
- * O(1) remove
+ * O(n) remove
  */
-export class FastMap {
-  private _valueStore: any[] = [];
+export class FastMap<T> {
+  private _valueStore: T[] = [];
   private _indexStore: number[] = [];
   private _keys: number[] = [];
-  private _values: any[] = [];
+  private _values: T[] = [];
+  private _keyField: string = 'id';
+  private _errors = {
+    keyField: `by default FastMap expects key to be 'id' field of element object`,
+    exists: `This element is already exists`
+  };
+  public length = 0;
 
-  constructor() {}
+  constructor(list?: T[], keyField: string = 'id') {
+    this._keyField = keyField;
 
-  public insert(key: number, value: any) {
+    if (list) {
+      list.forEach(item => {
+        const key = item[this._keyField];
+        if (!key) {
+          throw Error(this._errors.keyField);
+        }
+        this.insert(item, key);
+      });
+    }
+  }
+
+  /**
+   * if only value passed, then as a key value[keyField] will be used. keyField by default equals 'id'
+   */
+  public insert(value: T, key?: number) {
+    if (typeof key === 'undefined') {
+      if (!value[this._keyField]) {
+        throw Error(this._errors.keyField);
+      }
+      key = value[this._keyField];
+    }
+    if (this.read(key)) {
+      console.error(this._errors.exists, value, this.values());
+      return this.length;
+    }
     this._valueStore[key] = value;
     this._keys.push(key);
     this._values.push(value);
     this._indexStore[key] = this._keys.length - 1;
+    this._indexStore.length++;
+
+    return this.length;
   }
 
-  public update(key: number, newValue: any) {
+  /**
+   * if only value passed, then as a key value[keyField] will be used. keyField by default equals 'id'
+   */
+  public update(newValue: T, key?: number) {
+    if (typeof key === 'undefined') {
+      if (!newValue[this._keyField]) {
+        throw Error(this._errors.keyField);
+      }
+      key = newValue[this._keyField];
+    }
     this._valueStore[key] = newValue;
     const index = this._indexStore[key];
     typeof index !== 'undefined' && (this._values[index] = newValue);
@@ -32,19 +75,19 @@ export class FastMap {
   }
 
   public remove(key: number) {
-    this._valueStore[key] = undefined;
     const index = this._indexStore[key];
-    typeof index !== 'undefined' &&
-      this._keys.splice(index, 1) &&
+    if (typeof index !== 'undefined') {
+      this._keys.splice(index, 1);
       this._values.splice(index, 1);
-  }
-
-  public size() {
-    return this._keys.length;
+      this._valueStore[key] = undefined;
+      this._indexStore[key] = undefined;
+      this.adjustIndexValues(index);
+      this.length++;
+    }
   }
 
   public forEach(
-    iterator: (key: number, value: any, index: number) => boolean | undefined
+    iterator: (key: number, value: T, index: number) => boolean | undefined
   ) {
     for (let i = 0; i < this._keys.length; i++) {
       const key = this._keys[i];
@@ -62,5 +105,12 @@ export class FastMap {
 
   public values() {
     return this._values;
+  }
+
+  private adjustIndexValues(index: number) {
+    for (let i = index; i < this._keys.length; i++) {
+      const key = this._keys[i];
+      this._indexStore[key] = i;
+    }
   }
 }
